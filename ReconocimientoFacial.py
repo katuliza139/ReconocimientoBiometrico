edefc29e7f9082797342526f91f8d32f1a8ca3e9
katuliza139 <katyliza139@gmail.com>
2021-08-21 01:24:10 -0600

    Add files via upload

--- ReconocimientoFacial.py
+++ ReconocimientoFacial.py
@@ -9,10 +9,13 @@ face_recognizer = face_recognizer = cv2.face.LBPHFaceRecognizer_create()
 
 #leer modelo del entrenamiento
 face_recognizer.read('modeloEigenFace.xml')
+print("Lectura en proceso....")
 
-cap = cv2.VideoCapture('Imagenes y Videos de Prueba')
+cap = cv2.VideoCapture('comprobar1.mp4')
+#cap = cv2.VideoCapture('comprobar2.mp4')
+#cap = cv2.VideoCapture('comprobar3.mp4')
 
-faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+ 'haarcascade_frontalface_default.xml')
+faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 while True:
     ret, frame = cap.read()
@@ -27,7 +30,12 @@ while True:
         rostro = cv2.resize(rostro,(250,250), interpolation= cv2.INTER_CUBIC)
         result = face_recognizer.predict(rostro)
 
-        cv2.putText(frame, '{}'.format(result),(x,y + 5), 1, 1.3, (255,255,0),1,cv2.LINE_AA)
+        if result[0] < 60:
+            cv2.putText(frame,'{}'.format(imagePaths[result[0]]), (x,y + 5), 1, 1.3, (0,255,0),1,cv2.LINE_AA)
+            cv2.rectangle(frame,(x,y),(x+w,y+h), (0,255,0),2)
+        else:
+            cv2.putText(frame,'Desconocido', (x,y + 20), 2, 0.8, (0,0,255),1,cv2.LINE_AA)
+            cv2.rectangle(frame, (x,y),(x+w,y+h), (0,0,255),2)
 
     cv2.imshow('frame', frame)
     k = cv2.waitKey(1)
@@ -35,5 +43,5 @@ while True:
         break
 
 
-    cap.release()
-    cv2.destroyAllWindows()
+    #cap.release()
+    #cv2.destroyAllWindows()
